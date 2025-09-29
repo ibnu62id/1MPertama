c807c9fb9a9b93f6f4131889d58993d2b1faa97c
ibnu62id <ibnu62id@gmail.com>
2025-09-29 11:52:38 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,6 @@ C0 = st.number_input("Modal awal (Rp)", min_value=100, value=2000, step=100, for
 target = st.number_input("Target modal (Rp)", min_value=1_000_000, value=1_000_000_000, step=1_000_000, format="%0.0f")
 margin = st.slider("Margin keuntungan per transaksi (%)", 1, 100, 10)
 
-# Tampilkan dengan pemisah ribuan
-st.write("Modal awal yang dimasukkan:", f"Rp {C0:,.0f}")
-st.write("Target modal:", f"Rp {target:,.0f}")
-
 # Hitung jumlah langkah
 growth_factor = 1 + (margin/100)
 n = math.log(target/C0, growth_factor)
