7966bca754b4d1ec607200633ec03ca0e0b95379
ibnu62id <ibnu62id@gmail.com>
2025-09-29 11:51:34 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,14 @@ from io import BytesIO
 st.title("💰 Kalkulator")
 st.subheader("Rp2.000 Jadi Rp1 Miliar")
 
-# Input dari user
-C0 = st.number_input("Modal awal (Rp)", min_value=100, value=2000, step=100, format="%,d")
-target = st.number_input("Target pendapatan (Rp)", min_value=1000000, value=1000000000, step=1000000, format="%,d")
+# Input angka (biasa)
+C0 = st.number_input("Modal awal (Rp)", min_value=100, value=2000, step=100, format="%0.0f")
+target = st.number_input("Target modal (Rp)", min_value=1_000_000, value=1_000_000_000, step=1_000_000, format="%0.0f")
 margin = st.slider("Margin keuntungan per transaksi (%)", 1, 100, 10)
+
+# Tampilkan dengan pemisah ribuan
+st.write("Modal awal yang dimasukkan:", f"Rp {C0:,.0f}")
+st.write("Target modal:", f"Rp {target:,.0f}")
 
 # Hitung jumlah langkah
 growth_factor = 1 + (margin/100)
