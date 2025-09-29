d1d1d267b2faee4275a75c3189d742763dcfd2ca
ibnu62id <ibnu62id@gmail.com>
2025-09-29 11:43:14 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,12 @@ import pandas as pd
 import math
 from io import BytesIO
 
-st.title("💰 Kalkulator Rp2.000 Jadi Rp1 Miliar")
+st.title("💰 Kalkulator")
+st.title("Rp2.000 Jadi Rp1 Miliar")
 
 # Input dari user
 C0 = st.number_input("Modal awal (Rp)", min_value=100, value=2000, step=100)
-target = st.number_input("Target modal (Rp)", min_value=1000000, value=1000000000, step=1000000)
+target = st.number_input("Target pendapatan (Rp)", min_value=1000000, value=1000000000, step=1000000)
 margin = st.slider("Margin keuntungan per transaksi (%)", 1, 100, 10)
 
 # Hitung jumlah langkah
@@ -15,6 +16,5 @@ growth_factor = 1 + (margin/100)
 n = math.log(target/C0, growth_factor)
 steps = math.ceil(n)
 
-st.subheader("📊 Hasil Perhitungan")
-st.write(f"Idealnya, dengan modal awal **Rp{C0:,}** dan margin **{margin}%**, "
-         f"Anda hanya perlu **{steps} step** untuk mencapai Rp{target:,}.")
+st.subheader(f"📊 Idealnya, dengan modal awal **Rp{C0:,}** dan margin **{margin}%**, "
+         f"Anda hanya perlu **{steps} hari** untuk mencapai Rp{target:,}.")
